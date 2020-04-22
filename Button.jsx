57afd800592c16788f3163e1452f85b396138550
Pete Warman <petewarman@gmail.com>
2020-04-22 09:34:59 +0100

    add button styles

--- Button.jsx
+++ Button.jsx
@@ -4,5 +4,5 @@ import styles from "./button.module.css";
 export function Button(props) {
   props.className = `${styles.button} ${props.className}`;
 
-  return React.createElement("button", props);
+  return React.createElement("a", props);
 }
